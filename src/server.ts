208c2c9f98d390d6fc45b2961c31d123327a204e
Dency675 <neelamkavil2002@gmail.com>
2024-01-15 23:56:53 +0530

    added note apis

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import express from "express";
 import { sequelizeSync } from "./services/sequelize";
-import noteInformationRouter from '../src/router/note'
+import noteInformationRouter from "./router/note"
+import calculations from "./model/calculations";
 
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.port;
+
+sequelizeSync();
+
+app.use(express.urlencoded({ extended: true })); //to accept the encoded url
+
+app.use(express.json());
+
+app.use("/note", noteInformationRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, World hi!");
 });
-
-app.use("/note", noteInformationRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
