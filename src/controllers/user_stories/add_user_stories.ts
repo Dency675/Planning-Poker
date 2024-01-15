2987c755d24f75bdebabf6df974f5ad1eebbf309
Hariprasad-j21 <hariprasad.j21@gmail.com>
2024-01-15 17:38:29 +0530

    added add,get,update APIs for user_stories

--- src/controllers/user_stories/add_user_stories.ts
+++ src/controllers/user_stories/add_user_stories.ts
@@ -1,12 +1,30 @@
 import user_stories from "../../model/user_stories";
 import { Request, Response } from "express";
 
+/**
+ * oiouiytyuiyyiuoiy
+ * * uyfgh
+ * @param {Request} req - Express Request object containing client data.
+ * @param {Response} serveroiuyty - Express Response object for sending the server's response.
+ * @returns {Promise<void>} A JSON response indicating the success or failure of the operation.
+ */
 const add_user_stories = async (req: Request, res: Response): Promise<void> => {
-  const story = req.body as { story_title: string }[];
+  const story = req.body as { story_title: string };
 
   try {
-    if (!story || !Array.isArray(story)) {
+    if (!story) {
       res.status(404).json({ error: "Bad Request" });
     }
-  } catch {}
+    const data = await user_stories.create(
+      {
+        story_title: story.story_title,
+      },
+      { raw: true }
+    );
+    res.status(200).json({ message: "Data inserted successfully" });
+  } catch (error: any) {
+    res.status(500).json({ error: error.toString() });
+  }
 };
+
+export default add_user_stories;
