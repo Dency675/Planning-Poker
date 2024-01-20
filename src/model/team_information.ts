3be226ad7ac8c5029b4248d474b1c6c3d4dac9d3
geevarghese2000 <geevarghesemvarghese@gmail.com>
2024-01-20 18:33:24 +0530

    fix: change edit api from put to patch

--- src/model/team_information.ts
+++ src/model/team_information.ts
@@ -3,22 +3,24 @@ import TeamInformation from "../../types/modelTypes/team_information";
 import sequelize from "../config/sequelize-config";
 
 TeamInformation.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        team_name: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            unique:true,
+        },
+        status: {
+            type: DataTypes.ENUM('active', 'inactive'),
+            allowNull: false,
+        },
     },
-    team_name: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.ENUM("active", "inactive", "pending"),
-      allowNull: false,
-    },
-  },
+   
   {
     sequelize,
     modelName: "TeamInformation",
