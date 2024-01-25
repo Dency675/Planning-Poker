5ded3e81bca05d9e337bc44615bdaac556f9aedd
angelina-bit14 <angelnr108@gmail.com>
2024-01-25 15:27:08 +0530

    note-user mapping model and it's type created

--- src/controllers/note_user_session_mapping/get_controller.ts
+++ src/controllers/note_user_session_mapping/get_controller.ts
@@ -1,22 +1,22 @@
-import express from 'express';
-import { Request, Response, Router } from 'express';
+import express from "express";
+import { Request, Response, Router } from "express";
 
 note_associations();
 //const results = await MainModel.findAll({ include: [ JoinedModel ] }) as Array<MainModel & {JoinedModel: JoinedModel}>;
-import note_user_session_mapping from '../../model/note_user_session_mapping_Model';
-import NoteInformation from '../../model/note_information';
+import note_user_session_mapping from "../../model/note_user_session_mapping_Model";
+import NoteInformation from "../../model/note_information";
 // import session_participants from '../../model/session_participants';
 
 // export const getNoteUserMapping = async (req:Request,res:Response) => {
 //     try{
-          
+
 //         const {session_id , note_id} =req.query.params
 
 //         const value = await note_user_session_mapping.findOne({include:[{model: NoteInformation, attributes:['id'],as:'notes_id'},{model:session_participants ,attributes:['id'], as:'sessions_id'} ]},{where:{[Op.and]:{[sessions_id:session_id],[notes_id:note_id]}});
 //             else{
 //                 res.status(404).send('Error!\nNo mapping relations found...');
 //             }
-       
+
 //     }
 // catch(error){
 //    console.error(error);
