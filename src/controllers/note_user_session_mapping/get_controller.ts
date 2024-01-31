f1e54b79109f4586b95548ae3d7d635753d63fb8
angelina-bit14 <angelnr108@gmail.com>
2024-01-31 14:53:23 +0530

    commit

--- src/controllers/note_user_session_mapping/get_controller.ts
+++ src/controllers/note_user_session_mapping/get_controller.ts
@@ -5,24 +5,21 @@ note_associations();
 //const results = await MainModel.findAll({ include: [ JoinedModel ] }) as Array<MainModel & {JoinedModel: JoinedModel}>;
 import note_user_session_mapping from '../../model/note_user_session_mapping_Model';
 import NoteInformation from '../../model/note_information';
-
-console.log("p");
 // import session_participants from '../../model/session_participants';
 
-// export const getNoteUserMapping = async (req:Request,res:Response) => {
-//     try{
-          
-//         const {session_id , note_id} =req.query.params
+export const getNoteUserMapping = async (req:Request,res:Response) => {
+    try{
+          const session_id="9078110";
+          const note_id = "78789"
+        // const {session_id , note_id} =req.query.params
 
-//         const value = await note_user_session_mapping.findOne({include:[{model: NoteInformation, attributes:['id'],as:'notes_id'},{model:session_participants ,attributes:['id'], as:'sessions_id'} ]},{where:{[Op.and]:{[sessions_id:session_id],[notes_id:note_id]}});
-//             else{
-//                 res.status(404).send('Error!\nNo mapping relations found...');
-//             }
+        const value = await note_user_session_mapping.findOne({include:[{model: NoteInformation, attributes:['id'],as:'notes_id'},{model:session_participants ,attributes:['id'], as:'sessions_id'} ]},{where:{[Op.and]:{[sessions_id:session_id],[notes_id:note_id]}});
+
        
-//     }
-// catch(error){
-//    console.error(error);
-//    res.status(500).send('Internal server error!');
-// }
+    }
+catch(error){
+   console.error(error);
+   res.status(500).send('Internal server error!');
+}
 
-// }
+}
