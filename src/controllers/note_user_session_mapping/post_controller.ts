5e858e3878b9b4bc78a9cd6195fa94871403a975
angelina-bit14 <angelnr108@gmail.com>
2024-02-01 13:59:28 +0530

    Commit

--- src/controllers/note_user_session_mapping/post_controller.ts
+++ src/controllers/note_user_session_mapping/post_controller.ts
@@ -1,18 +1,11 @@
-import express from 'express';
-import { Request, Response, Router } from 'express';
-import note_user_session_mapping from '../../model/note_user_session_mapping_Model';
+import express from "express";
+import { Request, Response, Router } from "express";
+import note_user_session_mapping from "../../model/note_user_session_mapping_Model";
 
-export const postNoteUserMapping = async (req:Request,res:Response) => {
-    try{
-        
-
-        
-            
-       
-    }
-catch(error){
-   console.error(error);
-   res.status(500).send('Internal server error!');
-}
-
-}
+export const postNoteUserMapping = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error!");
+  }
+};
